b64c73c58a6f9d0636901b807a2bfa25509ded28
Camila Tavares <89873993+tcvmilvv@users.noreply.github.com>
2025-01-22 14:14:53 -0300

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,8 +6,7 @@ makedocs(
     sitename="SI: Beyond the Protein Shell: Understanding the Light-Material Composition of Zika Virus",
     repo = "https://github.com/tcvmilvv/ZIKV",
     pages=[
-	   "Home" => "docs/src/index.md",
-	   "Introduction" => "docs/src/index.md",
+	   "Introduction" => "index.md",
 	   "Data" => "data/data.md",
 	   "Scripts" => "scripts/scripts.md",
 	   "Plots" => "plots/plots.md"
